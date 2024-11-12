979748065f4423292bd17d820072cc9d736c1e11
Daniel Est <daniel.anth.est.03@gmail.com>
2024-11-12 12:13:01 +0800

    HTML

--- api/index.js
+++ api/index.js
@@ -9,9 +9,16 @@ app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
+/* 
 app.get('/', (request, response) => {
     response.status(200).json({ message: 'API Homepage' });
 });
+*/
+
+//Home Page
+app.get('/', (request, response) => {
+    response.send('<h1>API Homepage by Senior Dev Daniel Estrella </h1>');
+  });
 
 // Customers  
 const customerRouter = require('./Routes/customer');  
